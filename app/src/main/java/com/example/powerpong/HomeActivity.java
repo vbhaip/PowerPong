9e97be5f8543492df07a4004c371a02a5dcd9f9a
vbhaip <vbhaip01@gmail.com>
2019-04-29 15:32:01 -0400

    initial commit

---
+++ app/src/main/java/com/example/powerpong/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.powerpong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mUser = mAuth.getCurrentUser();
+
+        Log.d("dfasfal", mUser.getEmail());
+
+    }
+}
