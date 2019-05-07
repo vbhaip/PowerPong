1eabc5a4831bbd4d26f1c477d2fc4b194c5366ad
vbhaip <vbhaip01@gmail.com>
2019-05-07 14:12:48 -0400

    added formatting and working login screen

--- app/src/main/java/com/example/powerpong/HomeActivity.java
+++ app/src/main/java/com/example/powerpong/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.powerpong;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,13 +16,15 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_home);
 
         mAuth = FirebaseAuth.getInstance();
 
         mUser = mAuth.getCurrentUser();
 
-        Log.d("dfasfal", mUser.getEmail());
+//        Log.d("dfasfal", mUser.getEmail());
 
     }
 }
